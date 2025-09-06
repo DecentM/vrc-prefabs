fda6aa7c83e69353dc8fdfe07b20d6ed82dfd641
DecentM <decentm@decentm.com>
2025-09-06 08:10:50 +0300

    chore: restore temp removed scripts

---
+++ Assets/DecentM/Notification/Runtime/Scripts/PerformanceLevelChangeProvider.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using DecentM.PerformanceObserver;
+using DecentM.PerformanceObserver.Plugins;
+
+namespace DecentM.Notification.Providers
+{
+    public class PerformanceLevelChangeProvider : PerformanceObserverPlugin
+    {
+        public Notification notifications;
+        public Toggle toggle;
+
+        public Sprite iconHigh;
+        public Sprite iconMed;
+        public Sprite iconLow;
+
+        protected override void OnPerformanceModeChange(PerformanceObserverMode mode, float fps)
+        {
+            switch (mode)
+            {
+                case PerformanceObserverMode.Low:
+                    this.OnPerformanceLow();
+                    return;
+
+                case PerformanceObserverMode.Medium:
+                    this.OnPerformanceMedium();
+                    return;
+
+                case PerformanceObserverMode.High:
+                    this.OnPerformanceHigh();
+                    return;
+            }
+        }
+
+        private void OnPerformanceHigh()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconHigh, "Performance mode changed to High");
+        }
+
+        private void OnPerformanceMedium()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconMed, "Performance mode changed to Medium");
+        }
+
+        private void OnPerformanceLow()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconLow, "Performance mode changed to Low");
+        }
+    }
+}
